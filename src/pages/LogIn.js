d9f395e0e6735a003c165f07af2ee5a10a7e47e3
Pastas-dono <arospars77@gmail.com>
2023-07-06 10:56:50 +0200

    ✨feature: add LogIn page

---
+++ src/pages/LogIn.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import "./Login.scss";
+
+function LogIn() {
+    const [errUsername, setErrUsername] = useState(false);
+    const [errEmail, setErrEmail] = useState(false);
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:5000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    password,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                alert("You have successfully logged in");
+                setErrUsername(false);
+                setErrEmail(false);
+                console.log(data);
+            } else if (response.status === 400) {
+                const error = await response.json();
+                if (error.message.includes("Email")) {
+                    setErrEmail(true);
+                    setErrUsername(false);
+                } else if (error.message.includes("Username")) {
+                    setErrUsername(true);
+                    setErrEmail(false);
+                }
+                alert(error.message);
+            } else {
+                const error = await response.json();
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="container">
+            <h1 className="title">Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <div className={`field ${errUsername ? 'has-error' : ''}`}>
+                    <label className={`label ${errUsername ? 'error-label' : ''}`}>
+                        Username
+                    </label>
+                    <div className="control">
+                        <input
+                            className={`input ${errUsername ? 'error-input' : ''}`}
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            required
+                            onChange={(event) => setUsername(event.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className={`field ${errEmail ? 'has-error' : ''}`}>
+                    <label className={`label ${errEmail ? 'error-label' : ''}`}>
+                        Email
+                    </label>
+                    <div className="control">
+                        <input
+                            className={`input ${errEmail ? 'error-input' : ''}`}
+                            type="email"
+                            placeholder='Email'
+                            value={email}
+                            required
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Password</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            required
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LogIn;
